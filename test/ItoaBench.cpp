6305a41191c6cff84c4538540615ce8f0bfb0e66
ichenq <ichenq@gmail.com>
2014-08-25 11:32:43 +0800

    itoa benchmark example

---
+++ test/ItoaBench.cpp
@@ -0,0 +1,81 @@
+#include "Benchmark.h"
+#include "Logging.h"
+#include "Itoa.h"
+
+BENCHMARK(ItoaSprintf, n)
+{
+    char buf[20];
+    u32toa_sprintf(n, buf);
+
+    uint32_t r = 0;
+    BENCHMARK_SUSPEND
+    {
+        r = atoi(buf);
+    }
+    CHECK(r == n);
+}
+
+BENCHMARK_RELATIVE(ItoaItoa, n)
+{
+    char buf[20];
+    _itoa(n, buf, 10);
+
+    uint32_t r = 0;
+    BENCHMARK_SUSPEND
+    {
+        r = atoi(buf);
+    }
+    CHECK(r == n);
+}
+
+BENCHMARK_RELATIVE(ItoaMwilson, n)
+{
+    char buf[20];
+    u32toa_mwilson(buf, n);
+
+    uint32_t r = 0;
+    BENCHMARK_SUSPEND
+    {
+        r = atoi(buf);
+    }
+    CHECK(r == n);
+}
+
+BENCHMARK_RELATIVE(ItoaNaive, n)
+{
+    char buf[20];
+    u32toa_naive(n, buf);
+
+    uint32_t r = 0;
+    BENCHMARK_SUSPEND
+    {
+        r = atoi(buf);
+    }
+    CHECK(r == n);
+}
+
+BENCHMARK_RELATIVE(ItoaCount, n)
+{
+    char buf[20];
+    u32toa_count(n, buf);
+
+    uint32_t r = 0;
+    BENCHMARK_SUSPEND
+    {
+        r = atoi(buf);
+    }
+    CHECK(r == n);
+}
+
+BENCHMARK_RELATIVE(ItoaLut, n)
+{
+    char buf[20];
+    u32toa_lut(n, buf);
+
+    uint32_t r = 0;
+    BENCHMARK_SUSPEND
+    {
+        r = atoi(buf);
+    }
+    CHECK(r == n);
+}
