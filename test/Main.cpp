8615439e6f2af69d1d4cedcc829cece134f7f803
ichenq <ichenq@gmail.com>
2014-09-01 14:19:01 +0800

    do not check benchmark flags

--- test/Main.cpp
+++ test/Main.cpp
@@ -7,15 +7,8 @@ using std::endl;
 
 int main(int argc, char* argv[])
 {
-    FLAGS_benchmark = true;
-
-    // benchmarks
-    if (FLAGS_benchmark)
-    {
-        cout << "\nPATIENCE, BENCHMARKS IN PROGRESS." << endl;
-        runBenchmarks();
-        cout << "MEASUREMENTS DONE." << endl;
-    }
-    
+    cout << "\nPATIENCE, BENCHMARKS IN PROGRESS." << endl;
+    runBenchmarks();
+    cout << "MEASUREMENTS DONE." << endl;
     return 0;
 }
