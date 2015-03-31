020963d1ce442a68cf0a4b154cf599552c8013da
ichenq <ichenq@gmail.com>
2015-03-31 14:55:35 +0800

    Update copyright

--- src/benchmark.h
+++ src/benchmark.h
@@ -1,7 +1,3 @@
-// Copyright (C) 2014 ichenq@gmail.com. All rights reserved.
-// Distributed under the terms and conditions of the Apache License.
-// See accompanying files LICENSE.
-
 /*
  * Copyright 2014 Facebook, Inc.
  *
