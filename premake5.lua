eac085c9680333ef93d902511cfead5f8bbee41e
ichenq <ichenq@gmail.com>
2015-04-28 18:44:56 +0800

    Update premake script

--- premake5.lua
+++ premake5.lua
@@ -3,21 +3,21 @@
 --
 
 solution 'cpp-bench'
-    configurations {'Debug', 'Release'}
-    language 'C++'
-    --flags {'ExtraWarnings'}
-    targetdir 'bin'
-    platforms {'x32','x64'}
+    configurations  {'Debug', 'Release'}
+    language        'C++'
+    flags           'ExtraWarnings'
+    targetdir       'bin'
 
-    configuration 'Debug'
-        defines { 'DEBUG' }
-        flags { 'Symbols' }
+    filter 'configurations:Debug'
+        defines     { 'DEBUG' }
+        flags       { 'Symbols' }
 
-    configuration 'Release'
-        defines { 'NDEBUG' }
-        flags { 'Symbols', 'Optimize' }
+    filter 'configurations:Release'
+        defines     { 'NDEBUG' }
+        flags       { 'Symbols' }
+        optimize    'On'
 
-    configuration 'vs*'
+    filter 'action:vs*'
         defines
         {
             'WIN32',
@@ -28,17 +28,18 @@ solution 'cpp-bench'
             '_WINSOCK_DEPRECATED_NO_WARNINGS',
             'NOMINMAX',
         }
-    configuration 'gmake'
-        buildoptions '-std=c++11'
-        defines
-        {
-            '__STDC_LIMIT_MACROS',
-        }
-        links 'rt'
-        
+
+    filter 'action:gmake'
+        buildoptions    '-std=c++11'
+        defines         '__STDC_LIMIT_MACROS'
+
+    filter 'system:linux'
+        links   'rt'
+
     project 'benchmark_itoa'
-        location 'build'
-        kind 'ConsoleApp'
+        targetname  'benchmark_itoa'
+        location    'build'
+        kind        'ConsoleApp'
         files
         {
             'src/*.h',
